d8309261acf4671764e9c35177f8333915a3c24c
DewRat <120363612+devvratmiglani@users.noreply.github.com>
2023-06-12 01:20:15 +0530

    Create quoraLoginBox.js
    Use extension Scripty in chromium browsers or use bookmarklet.
    Scripty makes it automatic.
    Bookmarklet is manual.
    
    Scripty: https://chrome.google.com/webstore/detail/scripty-javascript-inject/milkbiaeapddfnpenedfgbfdacpbcbam
    
    Bookmarklet: https://www.freecodecamp.org/news/what-are-bookmarklets/

---
+++ quoraLoginBox.js
@@ -0,0 +1,11 @@
+javascript: (function() {
+    function F1(elem) {
+        document.querySelector('div[class="q-flex qu-alignItems--center qu-justifyContent--center qu-overflow--hidden qu-zIndex--blocking_wall"]').remove();
+        document.querySelector('div[style="box-sizing: border-box; filter: blur(3px);"]').setAttribute("style", "box-sizing: border-box; filter: blur(0px);");
+        var bodystyle = document.querySelector('body').getAttribute('style');
+        bodystyle = bodystyle.replaceAll(" ", "");
+        bodystyle = bodystyle.replace("overflow:hidden", "");
+        document.querySelector('body').setAttribute("style", bodystyle);
+    }
+    F1(1);
+})();
