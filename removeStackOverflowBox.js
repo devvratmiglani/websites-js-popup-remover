de8cb3beab5f173b456eba74971cd9d1d632d2e7
DewRat <120363612+devvratmiglani@users.noreply.github.com>
2023-06-12 01:34:28 +0530

    Update removeStackOverflowBox.js

--- removeStackOverflowBox.js
+++ removeStackOverflowBox.js
@@ -1,1 +1,2 @@
+//works for stackoverflow.co m
 javascript:document.querySelector('div[class="ff-sans ps-fixed z-nav-fixed ws4 sm:w-auto p32 sm:p16 bg-black-750 fc-white bar-lg b16 l16 r16 js-consent-banner"]').remove()
