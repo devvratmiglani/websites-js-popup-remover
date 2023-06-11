a4fa8a8777529d0adea6c7cc61143e1f5d9efef9
DewRat <120363612+devvratmiglani@users.noreply.github.com>
2023-06-12 01:32:04 +0530

    Add files via upload

---
+++ removeGfgLoginBox.js
@@ -0,0 +1,13 @@
+javascript:(function k()
+{
+    if(document.querySelector('div.login-modal-div'))
+    {
+      document.querySelector('div.spinner-loading-overlay').remove();
+      document.querySelector('div.login-modal-div').remove();
+      console.log("GfgTaskDone");
+    }
+  	else
+    {
+      setTimeout(k, 300);
+    }
+})();
